2d52f18182e0310ee8b2d45b6dac22dfc4250675
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "network_security_perimeters" {
   description = "All network_security_perimeter resources"
   value       = azurerm_network_security_perimeter.network_security_perimeters
 }
+output "network_security_perimeters_location" {
+  description = "List of location values across all network_security_perimeters"
+  value       = [for k, v in azurerm_network_security_perimeter.network_security_perimeters : v.location]
+}
+output "network_security_perimeters_name" {
+  description = "List of name values across all network_security_perimeters"
+  value       = [for k, v in azurerm_network_security_perimeter.network_security_perimeters : v.name]
+}
+output "network_security_perimeters_resource_group_name" {
+  description = "List of resource_group_name values across all network_security_perimeters"
+  value       = [for k, v in azurerm_network_security_perimeter.network_security_perimeters : v.resource_group_name]
+}
+output "network_security_perimeters_tags" {
+  description = "List of tags values across all network_security_perimeters"
+  value       = [for k, v in azurerm_network_security_perimeter.network_security_perimeters : v.tags]
+}
 
